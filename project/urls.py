829979a38d9b40fe23973e934f62785b1cfe5771
ogofe <7thogofe@gmail.com>
2025-11-26 12:31:34 +0100

    Remove dev-only folders from tracking

--- project/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from dj_palette.palette_admin.admin import palette_admin
-
-urlpatterns = [
-    path('admin/', palette_admin.urls),
-    path('old-admin/', admin.site.urls),
-]
